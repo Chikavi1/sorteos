b7af5b6701f9ff8c006293571ccc86eb9cbb2285
chikavi1 <chikavi@hotmail.com>
2024-03-05 22:39:57 -0600

    changes

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -16,7 +16,7 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule): void
     {
-        $schedule->command('app:free-status')->everyHour();
+        $schedule->command('app:free-status')->hourly();
     }
 
     /**

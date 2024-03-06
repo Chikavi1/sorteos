ae23e4c87cc47196bf05bb453cfe00ae7ad3a692
chikavi1 <chikavi@hotmail.com>
2024-03-05 22:53:43 -0600

    changes

--- app/Console/Commands/freeStatus.php
+++ app/Console/Commands/freeStatus.php
@@ -8,12 +8,13 @@ use Carbon\Carbon;
 
 class freeStatus extends Command
 {
+    protected $signature = 'demo:cron';
 
-    protected $signature = 'app:free-status';
 
     protected $description = 'Command description';
     public function handle()
     {
+
         $statusPending = Tickets::where('status',2)->where('created_at', '<',
         Carbon::now()->subHours(16)->toDateTimeString())->update(['status' => 1]);
 
--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -7,21 +7,12 @@ use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
 
 class Kernel extends ConsoleKernel
 {
-    protected $commands = [
-    App\Console\Commands\freeStatus::class,
-    ];
 
-    /**
-     * Define the application's command schedule.
-     */
     protected function schedule(Schedule $schedule): void
     {
-        $schedule->command('app:free-status')->hourly();
+        $schedule->command('demo:cron')->hourly();
     }
 
-    /**
-     * Register the commands for the application.
-     */
     protected function commands(): void
     {
         $this->load(__DIR__.'/Commands');

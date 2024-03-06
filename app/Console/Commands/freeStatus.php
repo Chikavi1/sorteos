070203c5e1e4948a25f45ea04be1de2b9db373a0
chikavi1 <chikavi@hotmail.com>
2024-03-05 22:33:21 -0600

    command

--- app/Console/Commands/freeStatus.php
+++ app/Console/Commands/freeStatus.php
@@ -8,27 +8,13 @@ use Carbon\Carbon;
 
 class freeStatus extends Command
 {
-    /**
-     * The name and signature of the console command.
-     *
-     * @var string
-     */
+
     protected $signature = 'app:free-status';
 
-    /**
-     * The console command description.
-     *
-     * @var string
-     */
     protected $description = 'Command description';
-
-    /**
-     * Execute the console command.
-     */
     public function handle()
     {
-        $statusPending = Tickets::where('status',2)
-        ->where('created_at', '<',
+        $statusPending = Tickets::where('status',2)->where('created_at', '<',
         Carbon::now()->subHours(16)->toDateTimeString())->update(['status' => 1]);
 
         dd($statusPending);
--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -7,12 +7,16 @@ use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
 
 class Kernel extends ConsoleKernel
 {
+    protected $commands = [
+        'App\Console\Commands\freeStatus',
+    ];
+
     /**
      * Define the application's command schedule.
      */
     protected function schedule(Schedule $schedule): void
     {
-        $schedule->command('app:free-status')->everyFourHours();
+        $schedule->command('app:free-status')->everyHour();
     }
 
     /**
